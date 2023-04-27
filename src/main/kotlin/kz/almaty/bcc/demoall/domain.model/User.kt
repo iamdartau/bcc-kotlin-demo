b1fa3ba25e9ecb14fecd0f21c4c0069ea305b758
Alexey Dartau <a.dartau@strongte.am>
2023-04-28 00:24:01 +0600

    while loop added

--- src/main/kotlin/kz/almaty/bcc/demoall/domain.model/User.kt
+++ src/main/kotlin/kz/almaty/bcc/demoall/domain.model/User.kt
@@ -3,17 +3,25 @@ package kz.almaty.bcc.demoall.domain.model
 class User {
     fun showInfo() {
         val const: Int = 1
-        var variable : Int = 2
+        var variable: Int = 2
         println("sum is ${const + variable}")
     }
 
-    fun iterate(){
+    fun iterate() {
         var unMutableList = listOf("apple", "melon", "mango") // we can't change it after init
         var mutableListOf = mutableListOf(1, 2, 3, 4, 5, 6)
         mutableListOf.add(7)
         println("unmutable list ${unMutableList} \nmutable list ${mutableListOf}")
-        for (item in mutableListOf){
+        for (item in mutableListOf) {
             println("${item + 10}")
         }
+        var x = 0
+        println("while loop")
+        while (x <= 5) {
+            println("${mutableListOf.get(x)}")
+            x++
+        }
     }
+
+
 }
